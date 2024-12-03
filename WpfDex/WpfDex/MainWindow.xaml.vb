d2f950b10ffbfd47c50257ed2ef96d394e30a7e1
Decker24 <lgdec350@learner.dvusd.org>
2024-12-03 13:04:59 -0700

    dex finished

--- WpfDex/WpfDex/MainWindow.xaml.vb
+++ WpfDex/WpfDex/MainWindow.xaml.vb
@@ -1,3 +1,28 @@
 ﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        FortniteBox.Visibility = Visibility.Visible
+        FortnitePic.Visibility = Visibility.Visible
+        SandcarBox.Visibility = Visibility.Hidden
+        SandcarPic.Visibility = Visibility.Hidden
+        K10Box.Visibility = Visibility.Hidden
+        K10Pic.Visibility = Visibility.Hidden
+    End Sub
 
+    Private Sub Sand_Click(sender As Object, e As RoutedEventArgs) Handles Sand.Click
+        FortniteBox.Visibility = Visibility.Hidden
+        FortnitePic.Visibility = Visibility.Hidden
+        SandcarBox.Visibility = Visibility.Visible
+        SandcarPic.Visibility = Visibility.Visible
+        K10Box.Visibility = Visibility.Hidden
+        K10Pic.Visibility = Visibility.Hidden
+    End Sub
+
+    Private Sub K10_Click(sender As Object, e As RoutedEventArgs) Handles K10.Click
+        FortniteBox.Visibility = Visibility.Hidden
+        FortnitePic.Visibility = Visibility.Hidden
+        SandcarBox.Visibility = Visibility.Hidden
+        SandcarPic.Visibility = Visibility.Hidden
+        K10Box.Visibility = Visibility.Visible
+        K10Pic.Visibility = Visibility.Visible
+    End Sub
 End Class
